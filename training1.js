3feb37c02f6c5a87b1870d336ff7de1884fe6f58
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-03-20 12:45:06 +0100

    exercises fundamentals3

---
+++ training1.js
@@ -0,0 +1,24 @@
+function add7(number) {
+    console.log(number + 7)
+}
+  
+add7(10)
+
+function multiply(a, b) {
+    console.log(a*b)
+}
+
+multiply(2, 9)
+
+
+function capitalize(string) {
+    console.log(string[0].toUpperCase() + string.slice(1).toLowerCase())
+}
+
+capitalize("YoU")
+
+function lastLetter(string) {
+    console.log(string.slice(-1))
+}
+
+lastLetter("abcd")
