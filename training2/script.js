737144ab06fc3d9ccc628f16b392fddda75e6882
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-04-10 09:34:14 +0200

    understanding of instructions

---
+++ training2/script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("#container");
+
+/* Create a red paragraphe in div-container */
+const p = document.createElement("p");
+p.style.color = 'red';
+p.textContent = "Hey I'm red!";
+container.appendChild(p);
+
+/* Create an h3 in div-container */
+
+
+/* Create a div-display in div-container */
+
+
+/* Create an h1 in div-display */
+
+
+/* Create a p in div-display */
