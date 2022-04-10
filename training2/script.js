e66b366f2c2c37c4d3f26860b53fb53d55b71aa3
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-04-10 20:28:35 +0200

    add new div

--- training2/script.js
+++ training2/script.js
@@ -13,9 +13,19 @@ h3.textContent = "I’m a blue h3!";
 container.appendChild(h3);
 
 /* Create a div-display in div-container */
-
+const display = document.createElement("div");
+display.style.backgroundColor = 'pink';
+display.style.border = '3px solid black';
 
 /* Create an h1 in div-display */
-
+const h1 = document.createElement("h1");
+h1.textContent = "I’m in a div.";
+display.appendChild(h1);
 
 /* Create a p in div-display */
+const p2 = document.createElement("p2");
+p2.textContent = "ME TOO!";
+display.appendChild(p2);
+
+
+container.appendChild(display);
